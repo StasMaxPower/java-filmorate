0e549be231494a55379a514b6fa97241a1b702b6
Your Name <shirobokovse@yandex.ru>
2022-06-30 21:51:42 +0400

    добавлена функциональность общие  v1.1

--- src/main/java/ru/yandex/practicum/filmorate/storage/filmStorage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/filmStorage/FilmDbStorage.java
@@ -34,8 +34,8 @@ public class FilmDbStorage implements FilmStorage{
         String sql = "SELECT f.film_id, f.name, f.description, f.duration, f.releasedate, f.rating, count(l.user_id) AS count_films " +
                 "FROM films AS f " +
                 "LEFT JOIN likes AS l ON f.film_id = l.film_id " +
-                "WHERE likes.user_id = ? and f.film_id in " +
-                "(select films.film_id where films.film_id = likes.film_id and likes.user_id = ?) " +
+                "WHERE l.user_id = ? and f.film_id in " +
+                "(select films.film_id from films where films.film_id = l.film_id and l.user_id = ?) " +
                 "GROUP BY f.film_id, f.name, f.description, f.duration, f.releasedate, f.rating " +
                 "ORDER BY count_films desc ";
         return jdbcTemplate.query(sql,(rs, rowNum) -> new Film(
