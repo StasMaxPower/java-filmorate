18565837689edfa1332f8333cbc0dd4a68868256
Your Name <shirobokovse@yandex.ru>
2022-07-03 11:56:17 +0400

    добавлена функциональность поиск v1.3

--- src/main/java/ru/yandex/practicum/filmorate/storage/filmStorage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/filmStorage/FilmDbStorage.java
@@ -37,16 +37,20 @@ public class FilmDbStorage implements FilmStorage {
     public List<Film> getBySearch(String query, List<String> by){
         String param1 = "";
         String param2 = "";
+        String param3 = "";
         if (by.contains("title")&&by.size()==1){
             param1 = "%" + query.toLowerCase() + "%";
-            param2 = "%";
+            param2 = "LOWER(f.name) LIKE ? and  (LOWER(directors.name) LIKE ? or directors.name is null)";
+            param3 = "%";
         }else if (by.contains("title")&&by.contains("director")&&by.size()==2){
             param1 = "%" + query.toLowerCase() + "%";
-            param2 = "%" + query.toLowerCase() + "%";
+            param2 = "LOWER(f.name) LIKE ? or LOWER(directors.name) LIKE ?";
+            param3 = "%" + query.toLowerCase() + "%";
 
         }else if (by.contains("director")&&by.size()==1){
             param1 = "%";
-            param2 = "%" + query.toLowerCase() + "%";
+            param2 = "(LOWER(f.name) LIKE ? or f.name is null)  and  LOWER(directors.name) LIKE ? ";
+            param3 = "%" + query.toLowerCase() + "%";
         }
 
         String sql = "select f.film_id, f.name, f.description, f.duration, f.releasedate, " +
@@ -54,9 +58,9 @@ public class FilmDbStorage implements FilmStorage {
                 " from films f " +
                 " left join FILM_DIRECTORS on f.FILM_ID = FILM_DIRECTORS.FILM_ID " +
                 " left join DIRECTORS on FILM_DIRECTORS.DIRECTOR_ID = DIRECTORS.DIRECTOR_ID "+
-                " where LOWER(f.name) LIKE  ?  and Lower(directors.name) LIKE ?  ";
+                " where " + param2;
 
-        return  jdbcTemplate.query(sql, (rs, rowNum) -> new Film(
+        List<Film> result =  jdbcTemplate.query(sql, (rs, rowNum) -> new Film(
                 rs.getInt("film_id"),
                 rs.getString("name"),
                 rs.getString("description"),
@@ -64,7 +68,9 @@ public class FilmDbStorage implements FilmStorage {
                 rs.getInt("duration"),
                 mpaStorage.getMpaToId(rs.getInt("rating")),
                 genreStorage.getGenres(rs.getInt("film_id")),
-                directorStorage.getDirectors(rs.getInt("film_id"))),param1, param2);
+                directorStorage.getDirectors(rs.getInt("film_id"))),param1, param3);
+         result.sort(Comparator.comparingInt(Film::getId).reversed());
+         return result;
     }
 
     @Override
